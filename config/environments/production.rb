2175cec471585c410040fa001cc40bacd05ffa85
Daniel Johnson <navseeker7@gmail.com>
2013-06-03 03:03:00 -0700

    fall back to precompiling if asset not found

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Zinlot::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -59,7 +59,7 @@ Zinlot::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += [/^[a-z0-9](\w|_|-|\.|\/(?!_))+\.(css|js)$/]
+  # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
