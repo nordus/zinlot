c1791647bb2039769370e273fde45cbb3107f27b
Daniel Johnson <navseeker7@gmail.com>
2013-06-10 17:56:51 -0700

    use dynamic carName for icon

--- app/controllers/live_location_controller.rb
+++ app/controllers/live_location_controller.rb
@@ -1,5 +1,6 @@
 class LiveLocationController < ApplicationController
   def index
-    @devices = Device.all
+    @devices = Device.includes(:vehicle).all
+    @car_names = @devices.each_with_object({}) {|d, hsh| hsh[d.id] = d.vehicle.car.name}
   end
 end
