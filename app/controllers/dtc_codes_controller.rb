894939e98a1e2b0989a40ae34ab68ab51a6c534e
Daniel Johnson <navseeker7@gmail.com>
2013-07-29 07:26:18 -0700

    allow CORS

--- app/controllers/dtc_codes_controller.rb
+++ app/controllers/dtc_codes_controller.rb
@@ -3,7 +3,21 @@ require 'open-uri'
 
 class DtcCodesController < ApplicationController
 
+  skip_before_filter :verify_authenticity_token
   respond_to :json
+
+  before_filter :allow_cors
+
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+        %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+
+    head(:ok) if request.request_method == "OPTIONS"
+    # or, render text: ''
+    # if that's more your style
+  end
 
   def show
     doc = Nokogiri::HTML(open("http://www.obd-codes.com/#{params[:code]}"))
