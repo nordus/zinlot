4ff8b7a593d9b96ca2d221d8fee340806a9ccc4d
Daniel Johnson <navseeker7@gmail.com>
2013-08-04 22:28:53 -0700

    change device_id to integer

--- app/controllers/live_location_controller.rb
+++ app/controllers/live_location_controller.rb
@@ -2,7 +2,7 @@ class LiveLocationController < ApplicationController
   def index
     @devices = Device.all
     @car_names = {}
-    @devices.each {|d| @car_names["#{d['id']}"] = "#{d['name']}"}
+    @devices.each {|d| @car_names["#{d['imei']}"] = "#{d['name']}"}
     @latest_readings = Reading.latest_readings
   end
 end
