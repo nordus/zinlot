e0f2e17964727e7246a7fb205c42225d5ac005fa
Daniel Johnson <navseeker7@gmail.com>
2013-08-06 13:33:01 -0700

    add UI for batch vehicle assignment

--- app/admin/vehicle.rb
+++ app/admin/vehicle.rb
@@ -1,3 +1,10 @@
 ActiveAdmin.register Vehicle do
 
+  for lot in DealerLot.all
+    batch_action "#{lot[:name]}: assign".to_sym do |selection|
+      Vehicle.find(selection).each {|v| p v}
+      redirect_to collection_path, :alert => "#{selection.length} Vehicles assigned to #{lot[:name]}"
+    end
+  end
+
 end
